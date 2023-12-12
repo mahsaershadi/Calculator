5263b581e3a5e52fe9482b92bc0a08fbb0640312
Mahdi Zargarnataj <m.zargarnataj@gmail.com>
2023-12-13 00:48:19 +0330

    اصلاحات جزیی

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -1,142 +1,145 @@
-using System.Windows.Forms;
-using System.IO;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
-namespace Calculator
+namespace Calculator;
+
+public partial class Form1 : Form
 {
-    public partial class Form1 : Form
+    private double resultValue = 0;
+    private string operationPerformed = "";
+    private bool isOperationOnPerformed = false;
+    private string FirstNum, SecondNum;
+
+    public Form1()
     {
-        double resultValue = 0;
-        string operationPerformed = "";
-        bool isOperationOnPerformed = false;
-        string FirstNum, SecondNum;
-        public Form1()
+        InitializeComponent();
+    }
+
+    private void tbResult_KeyPress(object sender, KeyPressEventArgs e)
+    {
+        if (!Char.IsDigit(e.KeyChar))
         {
-            InitializeComponent();
+            e.Handled = true;
+        }
+    }
+
+    private void Digit_Click(object sender, EventArgs e)
+    {
+        if (tbResult.Text.Equals("0") || isOperationOnPerformed)
+        {
+            tbResult.Clear();
         }
 
-        private void tbResult_KeyPress(object sender, KeyPressEventArgs e)
+        isOperationOnPerformed = false;
+        System.Windows.Forms.Button btn = (System.Windows.Forms.Button)sender;
+        if (btn.Text.Equals("."))
         {
-            if (!Char.IsDigit(e.KeyChar))
-            {
-                e.Handled = true;
-            }
-        }
-        private void Digit_Click(object sender, EventArgs e)
-        {
-            if ((tbResult.Text == "0") || (isOperationOnPerformed))
-            {
-                tbResult.Clear();
-            }
-
-            isOperationOnPerformed = false;
-            System.Windows.Forms.Button btn = (System.Windows.Forms.Button) sender;
-            if (btn.Text == ".")
-            {
-                if (!tbResult.Text.Contains("."))
-                    tbResult.Text += btn.Text;
-            }
-            else
-            {
+            if (!tbResult.Text.Contains('.'))
                 tbResult.Text += btn.Text;
-            }
         }
-
-        private void operator_Click(object sender, EventArgs e)
+        else
         {
-            System.Windows.Forms.Button btn1 = (System.Windows.Forms.Button)sender;
-            if (resultValue != 0)
-            {
-                btnEquel.PerformClick();
-                operationPerformed = btn1.Text;
-                labelShow.Text = resultValue + " " + operationPerformed;
-                isOperationOnPerformed = true;
-            }
-            else
-            {
-                operationPerformed = btn1.Text;
-                resultValue = Double.Parse(tbResult.Text);
-                labelShow.Text = resultValue + " " + operationPerformed;
-                isOperationOnPerformed = true;
-            }
-
-            FirstNum = labelShow.Text;
+            tbResult.Text += btn.Text;
         }
-        private void btnCE_Click(object sender, EventArgs e)
-        {
-            tbResult.Text = "0";
-        }
+    }
 
-        private void btnC_Click(object sender, EventArgs e)
+    private void operator_Click(object sender, EventArgs e)
+    {
+        System.Windows.Forms.Button btn1 = (System.Windows.Forms.Button)sender;
+        if (resultValue != 0)
         {
-            tbResult.Text = "0";
-            resultValue = 0;
+            btnEquel.PerformClick();
+            operationPerformed = btn1.Text;
+            labelShow.Text = resultValue + " " + operationPerformed;
+            isOperationOnPerformed = true;
         }
-        private void btnEquel_Click(object sender, EventArgs e)
+        else
         {
-            SecondNum = tbResult.Text;
-
-            switch (operationPerformed)
-            {
-                case "+":
-                    tbResult.Text = (resultValue + Double.Parse(tbResult.Text)).ToString();
-                    break;
-                case "-":
-                    tbResult.Text = (resultValue - Double.Parse(tbResult.Text)).ToString();
-                    break;
-                case "*":
-                    tbResult.Text = (resultValue * Double.Parse(tbResult.Text)).ToString();
-                    break;
-                case "/":
-                    tbResult.Text = (resultValue / Double.Parse(tbResult.Text)).ToString();
-                    break;
-                default:
-                    break;
-            }
+            operationPerformed = btn1.Text;
             resultValue = Double.Parse(tbResult.Text);
-            labelShow.Text = "";
-
-            //for history part
-            rtbHistory.AppendText(FirstNum + " " + SecondNum + " = " + "\n");
-            rtbHistory.AppendText("\t" + tbResult.Text + "\n\n");
-            lblHistoryText.Text = "";
-
+            labelShow.Text = resultValue + " " + operationPerformed;
+            isOperationOnPerformed = true;
         }
 
-        private void btnHistory_Click(object sender, EventArgs e)
+        FirstNum = labelShow.Text;
+    }
+
+    private void btnCE_Click(object sender, EventArgs e)
+    {
+        tbResult.Text = "0";
+    }
+
+    private void btnC_Click(object sender, EventArgs e)
+    {
+        tbResult.Text = "0";
+        resultValue = 0;
+    }
+
+    private void btnEquel_Click(object sender, EventArgs e)
+    {
+        SecondNum = tbResult.Text;
+
+        switch (operationPerformed)
         {
+            case "+":
+                tbResult.Text = (resultValue + Double.Parse(tbResult.Text)).ToString();
+                break;
 
-            btnDelete.Visible = !btnDelete.Visible;
-            btnDelete.BringToFront();
-            lblHistoryText.Visible = !lblHistoryText.Visible;
-            rtbHistory.Visible = !rtbHistory.Visible;
-            btnSave.Visible = !btnSave.Visible;
-            btnSave.BringToFront();
+            case "-":
+                tbResult.Text = (resultValue - Double.Parse(tbResult.Text)).ToString();
+                break;
 
+            case "*":
+                tbResult.Text = (resultValue * Double.Parse(tbResult.Text)).ToString();
+                break;
+
+            case "/":
+                tbResult.Text = (resultValue / Double.Parse(tbResult.Text)).ToString();
+                break;
+
+            default:
+                break;
         }
+        resultValue = Double.Parse(tbResult.Text);
+        labelShow.Text = string.Empty;
 
-        private void btnDelete_Click(object sender, EventArgs e)
+        //for history part
+        rtbHistory.AppendText(FirstNum + " " + SecondNum + " = " + "\n");
+        rtbHistory.AppendText("\t" + tbResult.Text + "\n\n");
+        lblHistoryText.Text = string.Empty;
+    }
+
+    private void btnHistory_Click(object sender, EventArgs e)
+    {
+        btnDelete.Visible = !btnDelete.Visible;
+        btnDelete.BringToFront();
+        lblHistoryText.Visible = !lblHistoryText.Visible;
+        rtbHistory.Visible = !rtbHistory.Visible;
+        btnSave.Visible = !btnSave.Visible;
+        btnSave.BringToFront();
+    }
+
+    private void btnDelete_Click(object sender, EventArgs e)
+    {
+        rtbHistory.Clear();
+        if (string.IsNullOrWhiteSpace(lblHistoryText.Text))
         {
-            rtbHistory.Clear();
-            if (lblHistoryText.Text == "")
+            lblHistoryText.Text = "there's no history yet";
+        }
+    }
+
+    //saving history data in txt file
+    private void btnSave_Click(object sender, EventArgs e)
+    {
+        SaveFileDialog saveFileDialog = new()
+        {
+            Filter = "Text File|*.txt"
+        };
+
+        if (saveFileDialog.ShowDialog() == DialogResult.OK)
+        {
+            // Save the history data to the specified text file
+            string filePath = saveFileDialog.FileName;
+            using (StreamWriter sw = new(filePath))
             {
-                lblHistoryText.Text = "there's no history yet";
-            }
-        }
-
-        //saving history data in txt file
-        private void btnSave_Click(object sender, EventArgs e)
-        {
-            SaveFileDialog saveFileDialog = new SaveFileDialog();
-            saveFileDialog.Filter = "Text File|*.txt";
-
-            if (saveFileDialog.ShowDialog() == DialogResult.OK)
-            {
-                // Save the history data to the specified text file
-                string filePath = saveFileDialog.FileName;
-                using (StreamWriter sw = new StreamWriter(filePath))
-                {
-                    sw.WriteLine(rtbHistory.Text);
-                }
+                sw.WriteLine(rtbHistory.Text);
             }
         }
     }
